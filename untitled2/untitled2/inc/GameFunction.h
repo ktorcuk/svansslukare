7533b4ca54d04896f2c903d38412fdfaa2e467fd
ktorcuk <158274311+ktorcuk@users.noreply.github.com>
2024-03-16 16:43:01 +0300

    Add files via upload

---
+++ untitled2/untitled2/inc/GameFunction.h
@@ -0,0 +1,11 @@
+//
+// Created by user on 15.03.2024.
+//
+
+#ifndef UNTITLED2_GAMEFUNCTION_H
+#define UNTITLED2_GAMEFUNCTION_H
+#include "ship.h"
+void GameFunction(MainShip* obj);
+void ScoreCalculator(MainShip* obj);
+
+#endif //UNTITLED2_GAMEFUNCTION_H
